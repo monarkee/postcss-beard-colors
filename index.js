837c2f83601b9058ab7313be2aa617d1554720e6
David Hemphill <davidlee.hemphill@gmail.com>
2017-09-01 15:34:12 -0500

    add svg fill color helpers

--- index.js
+++ index.js
@@ -2,32 +2,33 @@ var postcss = require('postcss')
 var _ = require('lodash')
 
 var prefixes = {
-  bg: 'background-color',
-  tc: 'color',
-  bc: 'border-color',
+    bg: 'background-color',
+    tc: 'color',
+    bc: 'border-color',
+    f: 'fill',
 }
 
 module.exports = postcss.plugin('postcss-beard-colors', function(opts) {
-  opts = opts || {}
+    opts = opts || {}
 
-  var colors = opts.colors
+    var colors = opts.colors
 
-  return function(root, result) {
-    var rules = _.flatMap(prefixes, function(declProp, prefixKey) {
-      return _.flatMap(colors, function(colorValue, colorKey) {
-        return postcss
-          .rule({
-            selector: `.${prefixKey}${colorKey}`,
-          })
-          .append(
-            postcss.decl({
-              prop: declProp,
-              value: colorValue,
+    return function(root, result) {
+        var rules = _.flatMap(prefixes, function(declProp, prefixKey) {
+            return _.flatMap(colors, function(colorValue, colorKey) {
+                return postcss
+                    .rule({
+                        selector: `.${prefixKey}${colorKey}`,
+                    })
+                    .append(
+                        postcss.decl({
+                            prop: declProp,
+                            value: colorValue,
+                        })
+                    )
             })
-          )
-      })
-    })
+        })
 
-    root.append(rules)
-  }
+        root.append(rules)
+    }
 })
